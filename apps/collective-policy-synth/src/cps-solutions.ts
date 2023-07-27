ada130d867c5473e8213c87991f0ad16de63c537
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-07-27 02:50:32 +0000

    Work on CPS

--- apps/collective-policy-synth/src/cps-solutions.ts
+++ apps/collective-policy-synth/src/cps-solutions.ts
@@ -9,11 +9,14 @@ import '@material/web/iconbutton/standard-icon-button.js';
 import '@material/web/select/outlined-select.js';
 import '@material/web/select/select-option.js';
 import '@material/web/iconbutton/outlined-icon-button.js';
+import '@material/web/chips/input-chip.js';
+
 import { MdOutlinedSelect } from '@material/web/select/outlined-select.js';
 
 @customElement('cps-solutions')
 export class CpsSolutions extends CpsStageBase {
   @property({ type: Boolean }) isDropdownVisible = false;
+  @property({ type: String }) searchText = '';
 
   async connectedCallback() {
     super.connectedCallback();
@@ -156,6 +159,20 @@ export class CpsSolutions extends CpsStageBase {
     ];
   }
 
+  get filteredSolutions() {
+    let subProblem = this.memory.subProblems[this.activeSubProblemIndex];
+    let solutions = subProblem.solutions.populations[this.activePopulationIndex];
+
+    if (this.searchText) {
+      solutions = solutions.filter(solution =>
+        solution.title.includes(this.searchText) ||
+        solution.description.includes(this.searchText)
+      );
+    }
+
+    return solutions;
+  }
+
   render() {
     const subProblems = this.memory.subProblems || [];
     if (this.activeSolutionIndex !== null) {
@@ -179,7 +196,7 @@ export class CpsSolutions extends CpsStageBase {
         <div class="title">${this.t('Evolving Solutions')}</div>
         <div class="generationContainer layout vertical center-center">
           ${this.renderChipSet(subProblem)}
-          ${subProblem.solutions.populations[this.activePopulationIndex].map(
+          ${this.filteredSolutions.map(
             (solution, index) =>
               html`<div
                 class="solutionItem"
@@ -217,6 +234,11 @@ export class CpsSolutions extends CpsStageBase {
           lastItems,
           subProblem.solutions.populations.length - 3
         )}
+         <md-input-chip
+          .label="${this.t('Search')}"
+          value=${this.searchText}
+          @change=${(e: Event) => this.searchText = (e.target as HTMLInputElement).value}
+        ></md-input-chip>
       </md-chip-set>
     `;
   }
@@ -296,16 +318,14 @@ export class CpsSolutions extends CpsStageBase {
   }
 
   renderSolutionScreen(solutionIndex: number) {
-    const solutions =
-      this.memory.subProblems[this.activeSubProblemIndex].solutions.populations[
-        this.activePopulationIndex
-      ];
+    const solutions = this.filteredSolutions;
     const solution = solutions[solutionIndex];
     return html`
       <div class="topContainer layout vertical center-center">
         <div class="layout horizontal center-center">
           <md-standard-icon-button
             aria-label="Previous"
+            .disabled="${solutionIndex === 0}"
             @click="${(): void => {
               if (solutionIndex > 0) {
                 this.activeSolutionIndex = solutionIndex - 1;
@@ -316,6 +336,7 @@ export class CpsSolutions extends CpsStageBase {
           </md-standard-icon-button>
           <md-standard-icon-button
             aria-label="Next"
+            .disabled="${solutionIndex === solutions.length - 1}"
             @click="${(): void => {
               if (solutionIndex < solutions.length - 1) {
                 this.activeSolutionIndex = solutionIndex + 1;
