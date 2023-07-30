a065bfd21b34959def68036b83ecacf15f53438c
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-07-30 05:32:43 +0000

    Work on Policy Synth

--- apps/collective-policy-synth/rollup.config.js
+++ apps/collective-policy-synth/rollup.config.js
@@ -4,6 +4,8 @@ import babel from '@rollup/plugin-babel';
 import html from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 import { terser } from 'rollup-plugin-terser';
+//import { generateSW } from 'rollup-plugin-workbox';
+//import analyze from 'rollup-plugin-analyzer';
 import replace from '@rollup/plugin-replace';
 import pkg from './package.json';
 const commonjs = require("rollup-plugin-commonjs");
@@ -15,6 +17,7 @@ function getCustomVersion(version) {
 
   return `Built on ${formattedDate} CET`;
 }
+
 
 export default {
   input: 'index.html',
@@ -30,27 +33,25 @@ export default {
   plugins: [
     commonjs(),
 
+    /** Enable using HTML as rollup entrypoint */
     html({
       minify: true,
+//        publicPath: '/'
+//      injectServiceWorker: true,
+//      serviceWorkerPath: 'dist/sw.js',
     }),
-
     copy({
       targets: [
         { src: 'locales', dest: 'dist/' }
       ]
     }),
-
+    /** Resolve bare module imports */
     nodeResolve(),
-
+    /** Minify JS */
     terser(),
-
+    /** Bundle assets references via import.meta.url */
     importMetaAssets(),
-
-    replace({
-      preventAssignment: true,
-      __VERSION__: getCustomVersion(pkg.version),
-    }),
-
+    /** Compile JS to a lower language target */
     babel({
       babelHelpers: 'bundled',
       presets: [
@@ -83,8 +84,28 @@ export default {
               minifyCSS: true,
             },
           },
+
         ],
       ],
     }),
+    replace({
+      preventAssignment: true,
+      __VERSION__: getCustomVersion(pkg.version),
+    }),
+    //analyze({summaryOnly: true})
+    /** Create and inject a service worker */
+    //generateSW({
+    //  globIgnores: ['polyfills/*.js', 'nomodule-*.js'],
+    //  navigateFallback: '/index.html',
+      // where to output the generated sw
+    //  swDest: path.join('dist', 'sw.js'),
+      // directory to match patterns against to be precached
+    //  globDirectory: path.join('dist'),
+      // cache any html js and css by default
+   //   globPatterns: ['**/*.{html,js,css,webmanifest}'],
+   //   skipWaiting: true,
+   //   clientsClaim: true,
+    //  runtimeCaching: [{ urlPattern: 'polyfills/*.js', handler: 'CacheFirst' }],
+    //}),
   ],
 };
