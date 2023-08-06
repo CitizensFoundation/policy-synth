bd41b460e8937a26b5d88acdb9cfc3896b0180ef
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-08-06 17:51:50 +0000

    Work on Policy Synth

--- apps/collective-policy-synth/src/CpsServerApi.ts
+++ apps/collective-policy-synth/src/CpsServerApi.ts
@@ -13,7 +13,7 @@ export class CpsServerApi extends YpServerApi {
   ): Promise<CpsBootResponse> {
     return (await this.fetchWrapper(
       this.baseUrlPath +
-        `/projects/${id}${forceGetBackupForProject ? `/true` : ``}${
+        `/projects/${id}${forceGetBackupForProject ? `?forceBackupReloadId=true` : ``}${
           tempPassword ? `?trm=${tempPassword}` : ''
         }`
     )) as unknown as CpsBootResponse;
