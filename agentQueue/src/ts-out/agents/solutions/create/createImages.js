a0e1d26f39784e39c03fe0fd67c173e442bf6476
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-10-10 22:15:48 +0000

    Work on Policy Synth

--- agentQueue/src/ts-out/agents/solutions/create/createImages.js
+++ agentQueue/src/ts-out/agents/solutions/create/createImages.js
@@ -280,7 +280,12 @@ export class CreateSolutionImagesProcessor extends BaseProcessor {
                             ? "File downloaded successfully."
                             : "File download failed.");
                         await this.uploadImageToS3(process.env.S3_BUCKET_NAME, imageFilePath, s3ImagePath);
-                        newImageUrl = `${this.cloudflareProxy}/${s3ImagePath}`;
+                        if (process.env.DISABLE_CLOUDFLARE_IMAGE_PROXY) {
+                            newImageUrl = `https://${process.env.S3_BUCKET_NAME}.s3.amazonaws.com/${s3ImagePath}`;
+                        }
+                        else {
+                            newImageUrl = `${this.cloudflareProxy}/${s3ImagePath}`;
+                        }
                         solution.imageUrl = newImageUrl;
                         this.logger.debug(`New Image URL: ${newImageUrl}`);
                     }
