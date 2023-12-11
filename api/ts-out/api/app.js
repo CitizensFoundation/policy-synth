05ec0e9853c9756678bb33c994e5f1dc35c311ea
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-12-11 18:06:49 +0000

    Work on PS

--- api/ts-out/api/app.js
+++ api/ts-out/api/app.js
@@ -47,6 +47,7 @@ export class App {
         this.app.use(bodyParser.json());
         this.app.use(express.static(path.join(__dirname, "../../apps/policy-synth/dist")));
         this.app.use("/projects*", express.static(path.join(__dirname, "../../apps/policy-synth/dist")));
+        this.app.use("/crt*", express.static(path.join(__dirname, "../../apps/policy-synth/dist")));
         this.app.use("/webResearch*", express.static(path.join(__dirname, "../../apps/policy-synth/dist")));
         this.app.use("/policies*", express.static(path.join(__dirname, "../../apps/policy-synth/dist")));
         this.app.use("/solutions*", express.static(path.join(__dirname, "../../apps/policy-synth/dist")));
