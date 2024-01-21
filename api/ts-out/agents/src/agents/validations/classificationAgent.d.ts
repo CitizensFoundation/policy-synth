74f4feea7f048cb991fe36823aad1b3bf7195c49
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-01-21 17:50:53 +0000

    Refactoring for npm

--- api/ts-out/agents/src/agents/validations/classificationAgent.d.ts
+++
@@ -1,9 +0,0 @@
-import { PsBaseValidationAgent } from "./baseValidationAgent.js";
-export declare class PsClassificationAgent extends PsBaseValidationAgent {
-    private routes;
-    constructor(name: string, options?: PsBaseValidationAgentOptions);
-    addRoute(classification: string, agent: PsValidationAgent): void;
-    protected performExecute(): Promise<PsValidationAgentResult>;
-    protected afterExecute(result: PsValidationAgentResult): Promise<void>;
-}
-//# sourceMappingURL=classificationAgent.d.ts.map
