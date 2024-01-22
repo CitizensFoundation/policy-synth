97de77fcef19d35368451d267368c868c368bc07
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-01-22 23:56:38 +0000

    Work on API

--- examples/simple/webApi/dist/simpleChatBot.js
+++
@@ -1,6 +0,0 @@
-import { BaseChatBot } from "./baseChatBot.js";
-export class SimpleChatBot extends BaseChatBot {
-    renderSystemPrompt() {
-        return `You are an expert answering question only as emojis`;
-    }
-}
