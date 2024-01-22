315353a91b0831481fc1393e4596960d65d3b22c
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-01-22 01:55:14 +0000

    Work on examples

--- examples/simple/webApp/src/simple-policy-synth-example-app.ts
+++ examples/simple/webApp/src/simple-policy-synth-example-app.ts
@@ -1,5 +1,16 @@
-import { PolicySynthWebApp } from "@policysynth/webapp";
+import { PolicySynthWebApp } from '@policysynth/webapp';
+import { html, css, nothing, TemplateResult } from 'lit';
+import { property, customElement, query } from 'lit/decorators.js';
+import { Router } from '@lit-labs/router';
 
 @customElement('simple-policy-synth-example-app')
 export class SimplePolicySynthExampleApp extends PolicySynthWebApp {
+  router: Router = new Router(this, [
+    {
+      path: '/',
+      render: () => {
+        return html`<cps-home .memory="${this.currentMemory}"></cps-home>`;
+      },
+    },
+  ]);
 }
