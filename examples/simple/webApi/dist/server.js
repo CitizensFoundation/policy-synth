c2594c9cd08082cbe04ec9e6940f63bca92c36cb
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-01-22 23:56:38 +0000

    Work on API

--- examples/simple/webApi/dist/server.js
+++
@@ -1,7 +0,0 @@
-import { AnalyticsController, PolicySynthApiApp } from '@policysynth/api';
-import { SimpleChatController } from './controllers/simpleChatController.js';
-const app = new PolicySynthApiApp([
-    AnalyticsController,
-    SimpleChatController,
-], 5011);
-app.listen();
--- examples/simple/webApi/dist/simpleChatBot.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseChatBot } from "./baseChatBot.js";
-export declare class SimpleChatBot extends BaseChatBot {
-    renderSystemPrompt(): string;
-}
-//# sourceMappingURL=simpleChatBot.d.ts.map
