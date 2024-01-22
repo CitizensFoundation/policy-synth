c3a69c861b5d81a4a063c09cd00bc082857ac828
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-01-22 23:55:41 +0000

    Removed

--- examples/simple/webApp/ts-out/src/SimpleServerApi.d.ts
+++
@@ -1,6 +0,0 @@
-import { YpServerApi } from "@yrpri/webapp";
-export declare class SimpleChatServerApi extends YpServerApi {
-    constructor(urlPath?: string);
-    conversation(chatLog: PsSimpleChatLog[], wsClientId: string): Promise<void>;
-}
-//# sourceMappingURL=SimpleServerApi.d.ts.map
--- examples/simple/webApp/ts-out/src/simple-example-app.d.ts
+++
@@ -1,8 +0,0 @@
-import { PolicySynthWebApp } from '@policysynth/webapp';
-import { PsRouter } from '@policysynth/webapp/cmp/base/router/router.js';
-import './simple-chat-bot.js';
-export declare class SimpleExampleApp extends PolicySynthWebApp {
-    static get styles(): import("lit").CSSResult[];
-    router: PsRouter;
-}
-//# sourceMappingURL=simple-example-app.d.ts.map
