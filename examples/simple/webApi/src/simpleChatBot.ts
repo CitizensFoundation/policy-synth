610bf28d7cd6f800a24c8c5b927be7b187a782f1
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-01-23 00:30:22 +0000

    Work on examples

--- examples/simple/webApi/src/simpleChatBot.ts
+++ examples/simple/webApi/src/simpleChatBot.ts
@@ -1,6 +1,6 @@
-import { BaseChatBot } from "../baseChatBot.js";
+import { PsBaseChatBot } from '@policysynth/api';
 
-export class SimpleChatBot extends BaseChatBot {
+export class SimpleChatBot extends PsBaseChatBot {
   renderSystemPrompt(): string {
     return `You are an expert answering question only as emojis`;
   }
