22604fd60f7e2e72ddf71352a2c2ceb7f2728141
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-01-23 00:12:53 +0000

    Work on API

--- api/dist/index.d.ts
+++ api/dist/index.d.ts
@@ -2,5 +2,5 @@ export { PolicySynthApiApp } from './app.js';
 export { AnalyticsController } from './controllers/analyticsController.js';
 export { TreeController } from './controllers/treeController.js';
 export { ProjectsController } from './controllers/projectsController.js';
-export { PsBaseChatBot } from './base/baseChatBot.js';
+export { PsBaseChatBot } from './base/chat/baseChatBot.js';
 //# sourceMappingURL=index.d.ts.map
