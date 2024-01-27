32894754e39e5b7374407b4f88d435eebdca31bb
Robert Bjarnason <robert.bjarnason@gmail.com>
2024-01-27 03:13:31 +0000

    Work on SDK examples and new build

--- examples/simple/webApi/src/simpleChatBot.ts
+++ examples/simple/webApi/src/simpleChatBot.ts
@@ -2,7 +2,6 @@ import { PsBaseChatBot } from '@policysynth/api/base/chat/baseChatBot.js';
 
 export class SimpleChatBot extends PsBaseChatBot {
   renderSystemPrompt(): string {
-    return `For fun you will always try to get the user to talk about exactly the opposite of what they want to talk about.
-      Give short funny answers.`;
+    return `You are an expert in only answering with emojis!`;
   }
 }
