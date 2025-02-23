731d51cb476564214bfe28c356327eb58dcf61c8
Robert Bjarnason <robert.bjarnason@gmail.com>
2025-02-23 19:41:54 +0000

    Work on agents

--- agents/ts-out/webResearch/searchWeb.d.ts
+++
@@ -1,9 +0,0 @@
-import { PolicySynthAgentBase } from "../base/agentBase.js";
-export declare class BaseSearchWebAgent extends PolicySynthAgentBase {
-    seenUrls: Map<string, Set<string>>;
-    callSearchApi(query: string, numberOfResults: number): Promise<PsSearchResultItem[]>;
-    getQueryResults(queriesToSearch: string[], id: string, numberOfResults?: number): Promise<{
-        searchResults: PsSearchResultItem[];
-    }>;
-}
-//# sourceMappingURL=searchWeb.d.ts.map
